fca7e2a85afa96b6596fdcfd928e80b46623e58c
grisha <yakovlev@saturnind.ru>
2024-10-03 22:19:11 +0300

    Двойная отрисовка одной группы треугольников со смещением

---
+++ src/stories/6/24_PerspectiveViewMVP/index.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { PerspectiveViewMVP } from "./PerspectiveViewMVP.tsx";
+
+const meta = {
+  title: "Chapter/6/24 PerspectiveViewMVP",
+  component: PerspectiveViewMVP,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    isTemplate: { control: "boolean" },
+  },
+  args: { isTemplate: true },
+} satisfies Meta<typeof PerspectiveViewMVP>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const UseCase: Story = {
+  args: {
+    isTemplate: true,
+  },
+};
---
+++ src/stories/6/24_PerspectiveViewMVP/shaders/fragment.frag
@@ -0,0 +1,7 @@
+// Спецификатор точности
+precision mediump float;
+varying vec4 v_Color;
+
+void main() {
+    gl_FragColor = v_Color;
+}
