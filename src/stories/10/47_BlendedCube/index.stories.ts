82332a7ed0b34f2a7ac8133c4d88df2948e8ccde
grisha <yakovlev@saturnind.ru>
2024-11-16 13:16:26 +0300

    Прозрачный куб

---
+++ src/stories/10/47_BlendedCube/index.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { BlendedCube } from "./BlendedCube.tsx";
+
+const meta = {
+  title: "Chapter/10/47 BlendedCube",
+  component: BlendedCube,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    isTemplate: { control: "boolean" },
+  },
+  args: { isTemplate: true },
+} satisfies Meta<typeof BlendedCube>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const UseCase: Story = {
+  args: {
+    isTemplate: true,
+  },
+};
